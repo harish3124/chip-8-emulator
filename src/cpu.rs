f8849f257b856112fb93a3d08c2384136ab2f2e3
Harish <harishhari3112004@gmail.com>
2023-05-11 00:34:53 +0530

    Setup graphics and Cpu Structure

---
+++ src/cpu.rs
@@ -0,0 +1,36 @@
+use bevy::prelude::Resource;
+
+#[derive(Resource)]
+pub struct Cpu {
+    pub memory: [u8; 4096], // 4 KB Memory
+    pub opcode: u16,
+    pub V: [u8; 16], // General Registers
+    pub I: u16,      // Special Register
+    pub pc: u16,     // Program Counter
+    pub display: [[u8; 64]; 32],
+    pub delay_timer: u8,
+    pub sound_timer: u8,
+    pub stack: [u16; 16],
+    pub sp: u8, // Stack Pointer
+    pub keypad: [u8; 16],
+    pub redraw: bool,
+}
+
+impl Default for Cpu {
+    fn default() -> Self {
+        Self {
+            memory: [0; 4096],
+            opcode: 0,
+            V: [0; 16],
+            I: 0,
+            pc: 0,
+            display: [[0; 64]; 32],
+            delay_timer: 0,
+            sound_timer: 0,
+            stack: [0; 16],
+            sp: 0,
+            keypad: [0; 16],
+            redraw: false,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,94 @@
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+use cpu::Cpu;
+
+mod cpu;
+mod font;
+
+const WINDOW_SIZE: (f32, f32) = (640.0, 320.0);
+const TILE_SIZE: f32 = 10.0;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WINDOW_SIZE.into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::BLACK))
+        .init_resource::<Cpu>()
+        .add_startup_system(spawn_camera)
+        .add_system(remove_pixel)
+        .add_system(draw_pixel.after(remove_pixel))
+        .add_system(get_input)
+        .run()
+}
+fn remove_pixel(mut commands: Commands, pixels: Query<(Entity, &Pixel)>, cpu: Res<Cpu>) {
+    if cpu.redraw {
+        for (entity, pixel_ref) in pixels.iter() {
+            if cpu.display[pixel_ref.row][pixel_ref.col] == 0 {
+                commands.entity(entity).despawn();
+            }
+        }
+    }
+}
+
+fn draw_pixel(mut commands: Commands, mut cpu: ResMut<Cpu>) {
+    if cpu.redraw {
+        for row in 0..32 {
+            for col in 0..64 {
+                if cpu.display[row][col] == 1 {
+                    commands.spawn((
+                        SpriteBundle {
+                            sprite: Sprite {
+                                // Check if pixel active
+                                color: Color::WHITE,
+                                custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                                ..default()
+                            },
+                            // texture: asset_server.load("pixel.png"),
+                            transform: Transform::from_xyz(
+                                ((col as f32) * TILE_SIZE) + TILE_SIZE / 2.0,
+                                ((row as f32) * TILE_SIZE) + TILE_SIZE / 2.0,
+                                0.,
+                            ),
+                            ..default()
+                        },
+                        Pixel { row: row, col: col },
+                    ));
+                }
+            }
+        }
+        cpu.redraw = false;
+    }
+}
+
+#[derive(Component)]
+struct Pixel {
+    row: usize,
+    col: usize,
+}
+
+fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+}
+
+// DEBUG
+fn get_input(input: Res<Input<KeyCode>>, mut cpu: ResMut<Cpu>) {
+    if input.just_pressed(KeyCode::Up) {
+        if cpu.display[10][20] == 0 {
+            cpu.display[10][20] = 1;
+        } else {
+            cpu.display[10][20] = 0;
+        }
+        cpu.redraw = true;
+    }
 }
